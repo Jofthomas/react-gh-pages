0093e7609e856d5141da369d028165eca6ffc905
Joffrey THOMAS <joffrey.thomas@insead.edu>
2022-07-22 15:21:06 +0200

    Updates

---
+++ precache-manifest.7bd9b2c538f0a1de3984f9e2b6e76ba4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2632272e0fb179532c51dce5dda3d176",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ef2723c30a803777d86c",
+    "url": "/react-gh-pages/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "92638ad80f0543f126c1",
+    "url": "/react-gh-pages/static/css/main.6d2c3100.chunk.css"
+  },
+  {
+    "revision": "ef2723c30a803777d86c",
+    "url": "/react-gh-pages/static/js/2.bdafdd50.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/react-gh-pages/static/js/2.bdafdd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92638ad80f0543f126c1",
+    "url": "/react-gh-pages/static/js/main.1fad4611.chunk.js"
+  },
+  {
+    "revision": "167147c477b7f371334a",
+    "url": "/react-gh-pages/static/js/runtime-main.16fd55fc.js"
+  }
+]);
